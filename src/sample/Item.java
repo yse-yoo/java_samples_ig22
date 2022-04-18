03ef8d51c11329b01be3275d49cbc07b97063ccc
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-04-18 10:24:11 +0900

    update

--- src/sample/Item.java
+++ src/sample/Item.java
@@ -15,11 +15,20 @@ public class Item {
 		itemName = "コーヒー";
 		price = 120;
 		amount = 10;
-
+		is_sale = true;
+		
 		System.out.println(itemName);
 		System.out.println(price);
 		System.out.println(amount);
+		System.out.println(is_sale);
+		
+		//小数
+		float rate = 0.1f;
+		System.out.println(rate);
 
+		//定数（決まった数）
+		final float TAX_RATE = 0.8f;
+		System.out.println(TAX_RATE);
 	}
 
 }
---
+++ src/sample/ItemCalculate.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class ItemCalculate {
+
+	public static void main(String[] args) {
+		int answer = 1 + 2;
+		System.out.println(answer);
+
+
+	}
+
+}
