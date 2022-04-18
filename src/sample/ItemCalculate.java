5f3c5f7b8886963316008a021d033bbed64a72e6
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-04-18 10:51:12 +0900

    update cast

--- src/sample/ItemCalculate.java
+++ src/sample/ItemCalculate.java
@@ -5,8 +5,25 @@ public class ItemCalculate {
 	public static void main(String[] args) {
 		int answer = 1 + 2;
 		System.out.println(answer);
+		
+		answer = answer + 3;
+		answer = answer - 2;
+		answer = answer * 5;
+		answer = answer / 2;
 
+		System.out.println(answer);
 
+		int mod = answer % 3;
+		System.out.println(mod);
+
+		//ポイントの計算
+		int price = 200;
+		float pointRate = 0.05f;
+		float point = (float) price * pointRate;
+
+		System.out.println(point);
+		System.out.println(point + "pt");
+		System.out.println((int) point + "pt");
 	}
 
 }
