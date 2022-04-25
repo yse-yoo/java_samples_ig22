9bedd5d19eb393417dbdc80ea5a5fca810b10c23
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-04-25 12:09:58 +0900

    add hands

--- src/sample/Drink.java
+++ src/sample/Drink.java
@@ -1,6 +1,7 @@
 package sample;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Drink {
 
@@ -18,6 +19,22 @@ public class Drink {
 		drinks[1] = "ウーロン茶";
 		System.out.println(Arrays.toString(drinks));
 		System.out.println(drinks[1]);
+		
+
+		//配列の初期化（initialize)
+		int[] numbers = new int[3];
+		numbers[0] = 50;
+		numbers[1] = 25;
+		numbers[2] = 18;
+//		numbers[3] = 10;
+		System.out.println(Arrays.toString(numbers));
+		
+		//グー、チョキ、パー、Rock & Scissors & Paper
+		String[] hands = {"グー", "チョキ", "パー"};
+
+		//1 - 3 までの整数(せいすう)
+		Random rand = new Random();
+		int handIndex = rand.nextInt(3);
 
 	}
 
