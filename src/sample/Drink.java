c66f2ac2fd0163508bf955e98ffcc75e0ea6791c
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-04-25 11:47:27 +0900

    update array

--- src/sample/Drink.java
+++ src/sample/Drink.java
@@ -7,9 +7,17 @@ public class Drink {
 	public static void main(String[] args) {
 		
 		//配列（Array)
-		String[] drinks = {"コーヒ", "紅茶", "ほうじ茶"};
+		String[] drinks = {"コーヒー", "紅茶", "ほうじ茶"};
 		System.out.println(drinks);
 		System.out.println(Arrays.toString(drinks));
+		
+		//index = 1を選択
+		String selectDrink = drinks[1];
+		System.out.println(selectDrink);
+
+		drinks[1] = "ウーロン茶";
+		System.out.println(Arrays.toString(drinks));
+		System.out.println(drinks[1]);
 
 	}
 
