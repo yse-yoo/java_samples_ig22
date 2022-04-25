b64d6648d7b4bb2d2d20aa41a6ae351ee34039f1
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-04-25 11:37:52 +0900

    add class Drink

---
+++ src/sample/Drink.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.Arrays;
+
+public class Drink {
+
+	public static void main(String[] args) {
+		
+		//配列（Array)
+		String[] drinks = {"コーヒ", "紅茶", "ほうじ茶"};
+		System.out.println(drinks);
+		System.out.println(Arrays.toString(drinks));
+
+	}
+
+}
