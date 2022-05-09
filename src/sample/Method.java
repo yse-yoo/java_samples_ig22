ae3b6bc934ee47eb34eefa906ddf737ee60d85f9
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-09 11:48:05 +0900

    change static method

--- src/sample/Method.java
+++ src/sample/Method.java
@@ -9,10 +9,21 @@ public class Method {
 		answer = calculate(5f);
 		System.out.println(answer);
 
+		int totalPrice = 0;
+		totalPrice = calculateTotalPrice(100, 5);
+		System.out.println(totalPrice);
+
+		totalPrice = calculateTotalPrice(170, 7);
+		System.out.println(totalPrice);
 	}
 	
 	public static Float calculate(Float x) {
 		Float y = x + 8;
 		return y;
+	}
+	
+	public static int calculateTotalPrice(int price, int amount) {
+		int totalPrice = price * amount;
+		return totalPrice;
 	}
 }
