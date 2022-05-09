fb7813c031380aa8c48cc21a8089e70145496d70
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-09 09:26:56 +0900

    change ItemRepeat

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -3,7 +3,7 @@ package sample;
 public class ItemRepeat {
 
 	public static void main(String[] args) {
-		
+
 		int count = 0;
 
 		for (int i = 0; i <= 10; i++) {
@@ -13,6 +13,15 @@ public class ItemRepeat {
 
 //		System.out.println(i);
 		System.out.println("count is:" + count);
+
+		for (int x = 1; x <= 10; x++) {
+
+			for (int y = 1; y <= 10; y++) {
+
+			}
+
+		}
+
 	}
 
 }
