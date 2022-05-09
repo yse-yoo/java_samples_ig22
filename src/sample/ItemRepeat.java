f94507d3a514a74cf525bfb941173746f8716bf5
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-09 09:12:21 +0900

    add ItemRepeat.java

---
+++ src/sample/ItemRepeat.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class ItemRepeat {
+
+	public static void main(String[] args) {
+		
+		int count = 0;
+
+		for (int i = 0; i <= 10; i++) {
+			count++;
+			System.out.println(i);
+		}
+
+//		System.out.println(i);
+		System.out.println("count is:" + count);
+	}
+
+}
