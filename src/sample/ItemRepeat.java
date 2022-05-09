126a2a6905d94dd0b97c9a2c656600301f86c61a
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-09 09:32:37 +0900

    change ItemRepeat

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -17,9 +17,10 @@ public class ItemRepeat {
 		for (int x = 1; x <= 10; x++) {
 
 			for (int y = 1; y <= 10; y++) {
-
+				String plot = "(" + x + " , " + y + ")";
+				System.out.print(plot);
 			}
-
+			System.out.println();
 		}
 
 	}
