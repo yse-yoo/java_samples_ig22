920d086272c7ba4d32d8041eb3fd7040b3d8ba43
yse-yoo <71994236+yse-yoo@users.noreply.github.com>
2022-05-16 10:08:27 +0900

    add concole

--- src/rpg/App.java
+++ src/rpg/App.java
@@ -2,9 +2,14 @@ package rpg;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void man(String[] args) {
+		//インスタンス（instance）
 		Character wizard = new Character();
 		System.out.println(wizard);
+		
+		System.out.println(wizard.name);
+		System.out.println(wizard.job);
+		System.out.println(wizard.hp);
 	}
 
 }
